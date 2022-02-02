c1d9ef75baea1e4b33fa8a9ec040864565b4b03a
yong4s <yakhvan20116@gmail.com>
2022-02-02 19:50:10 +0200

    second commit

--- Parsetest/Parsetest/spiders/rozetkascr.py
+++ Parsetest/Parsetest/spiders/rozetkascr.py
@@ -1,20 +1,21 @@
 import scrapy
 from Parsetest.items import RozItem
 from scrapy.loader import ItemLoader
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class RozetkascrSpider(scrapy.Spider):
+class RozetkascrSpider(CrawlSpider):
     name = 'rozetkascr'
     start_urls = ['https://rozetka.com.ua/']
+    rules = (
+        Rule(
+            LinkExtractor(restrict_css=
+                          'div.menu-wrapper.menu-wrapper_state_static.ng-star-inserted')),
+        Rule(
+            LinkExtractor(restrict_css=
+                          'a.tile-cats__heading.tile-cats__heading_type_center.ng-star-inserted'), callback='parse_products'))
 
-
-    def parse(self, response):
-        for link in response.css('div.menu-wrapper.menu-wrapper_state_static.ng-star-inserted ul li a::attr(href)').extract():
-            yield response.follow(link, callback=self.parse_category)
-
-    def parse_category(self, response):
-        for link in response.css('a.tile-cats__heading.tile-cats__heading_type_center.ng-star-inserted::attr(href)').extract():
-            yield response.follow(link, callback=self.parse_products)
 
     def parse_products(self, response):
         for products in response.css('li.catalog-grid__cell.catalog-grid__cell_type_slim.ng-star-inserted'):
